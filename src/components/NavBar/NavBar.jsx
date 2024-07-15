7edc8fe384ebe061ac4363f541c7c28e8714af13
ivanbilenkyi <ivanbilenky@gmail.com>
2024-07-15 17:01:39 +0300

    fix styles navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,26 +3,29 @@ import { NavLink } from 'react-router-dom';
 export const NavBar = () => {
     return (
         <nav className='hidden lg:block'>
-            <ul className='flex items-center gap-px'>
-                <li className='bg-white rounded-60 border border-borderNavLink p-2 text-text'>
+            <ul className='flex items-center gap-[2px]'>
+                <li className='bg-white rounded-60 border border-borderNavLink p-2 text-text relative'>
                     <NavLink to='/' className={({ isActive }) =>
                         isActive ? 'bg-green rounded-60 text-white py-2 px-5 block' : 'block py-2 px-5'
                     }>
                         Home
                     </NavLink>
+                    <div className='absolute right-[-5px] bottom-[20px] w-[6px] h-[15px] bg-white border-t-[1px] border-b-[1px] border-l-[1px] border-r-[1px] border-t-borderNavLink border-b-borderNavLink border-l-white border-r-white z-[1]'></div>
                 </li>
-                <li className='bg-white rounded-60 border border-borderNavLink p-2 text-text'>
+                <li className='bg-white rounded-60 border border-borderNavLink p-2 text-text relative'>
                     <NavLink
                         to='/medicine-store'
-                        className={({ isActive }) =>
+                        className={({isActive}) =>
                             isActive ? 'bg-green rounded-60 text-white py-2 px-5 block' : 'block py-2 px-5'
                         }
                     >
                         Medicine Store
                     </NavLink>
+                    <div
+                        className='absolute right-[-5px] bottom-[20px] w-[6px] h-[15px] bg-white border-t-[1px] border-b-[1px] border-l-[1px] border-r-[1px] border-t-borderNavLink border-b-borderNavLink border-l-white border-r-white'></div>
                 </li>
                 <li className='bg-white rounded-60 border border-borderNavLink p-2 text-text'>
-                    <NavLink to='/medicine'  className={({ isActive }) =>
+                    <NavLink to='/medicine' className={({isActive}) =>
                         isActive ? 'bg-green rounded-60 text-white py-2 px-5 block' : 'block py-2 px-5'
                     }>
                         Medicine
